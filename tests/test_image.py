ab59d68d9cb7e733d5f60557ef0626fcb7ae67f3
Frotaur <frotaut@hotmail.co.uk>
2024-11-18 22:11:15 +0100

    Added auto-rescaled, fixed incorrect image size problems

--- tests/test_image.py
+++ tests/test_image.py
@@ -20,13 +20,16 @@ def test_view():
     transf = import_torchvision().transforms
     image = Image.open(os.path.join(curpath, "test_folder", "whale.png"))
 
-    whale = transf.ToTensor()(image)  # (3,H,W);
+    whale = transf.ToTensor()(image)  # (4,H,W);
     randAug = transf.RandomResizedCrop(whale.shape[-2:])
-    whale_tile = torch.stack([randAug(whale) for _ in range(9)], dim=0)  # (9,3,H,W)
+    whale_tile = torch.stack([randAug(whale) for _ in range(7)], dim=0)  # (9,3,H,W)
 
-    alpha_noise = torch.rand(9, 1, *whale.shape[-2:])
-    whale_tile = torch.cat([whale_tile, alpha_noise], dim=1)  # (10,4,H,W)
-    show_image(whale_tile, columns=5, colorbar=False, max_width=None, padding=0, pad_value=1.0)
+    alpha_noise = torch.rand(7, 1, *whale.shape[-2:])
+    whale_tile[:, 3:, :, :] = whale_tile[:, 3:, :, :] * (alpha_noise)  # (9,4,H,W)
+    num, _, H, W = whale_tile.shape
+    print("first, expected shape : ", (H * 2, W * 4))
+    show_image(whale_tile, columns=4, colorbar=False, max_width=None, padding=0, pad_value=1.0)
+    print("Second, expected max_width : 500, figure shape : ", (H * 2, W * 4))
     show_image(whale_tile[:, 2:3], columns=None, colorbar=True, max_width=500, padding=3, pad_value=0.0)
 
 
@@ -40,10 +43,13 @@ def test_save():
     transf = import_torchvision().transforms
     image = Image.open(os.path.join(curpath, "test_folder", "whale.png"))
 
-    whale = transf.ToTensor()(image)  # (3,H,W);
+    whale = transf.ToTensor()(image)  # (4,H,W);
     randAug = transf.RandomResizedCrop(whale.shape[-2:])
-    whale_tile = torch.stack([randAug(whale) for _ in range(9)], dim=0)  # (10,3,H,W)
+    whale_tile = torch.stack([randAug(whale) for _ in range(9)], dim=0)  # (9,4,H,W)
 
+    _, _, H, W = whale_tile.shape
+
+    print("Expected shape : ", (H * 2, W * 5))
     save_image(
         whale_tile,
         folder="test_folder",
@@ -55,7 +61,7 @@ def test_save():
         pad_value=1.0,
     )
     save_image(
-        whale_tile[:, 2:],
+        whale_tile[:, 2:3],
         folder="test_folder",
         name="dawhales",
         columns=None,
@@ -65,4 +71,7 @@ def test_save():
         pad_value=0.0,
     )
 
-test_save()
+
+# # test_save()
+# test_view()
+# test_save()
